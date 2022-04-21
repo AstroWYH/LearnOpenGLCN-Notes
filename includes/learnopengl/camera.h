c62adeeea291563012e7f89d373c1e3f407c33d1
wangyuhang <819369336@qq.com>
2022-04-22 02:06:00 +0800

    [add] OpenGL学习笔记-include

--- includes/learnopengl/camera.h
+++ includes/learnopengl/camera.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 
+#include <stdio.h>
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
     FORWARD,
@@ -20,7 +21,7 @@ const float YAW         = -90.0f;
 const float PITCH       =  0.0f;
 const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
-const float ZOOM        =  45.0f;
+const float ZOOM        =  45.0f; // wyh 45.0f比较接近真实, 末日废墟可调大
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
@@ -63,7 +64,7 @@ public:
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix()
     {
-        return glm::lookAt(Position, Position + Front, Up);
+        return glm::lookAt(Position, Position + Front, Up); // wyh 最终的关键公式
     }
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -121,10 +122,18 @@ private:
         front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
         front.y = sin(glm::radians(Pitch));
         front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        Front = glm::normalize(front);
-        // also re-calculate the Right and Up vector
+        Front = glm::normalize(front); // wyh 摄像机的Front
+        // also re-calculate the Right and Up vector // wyh 摄像机的Right
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
+        Up    = glm::normalize(glm::cross(Right, Front)); // wyh 摄像机的Up
+        // for (int i = 0; i < 20; i++) {
+            // printf("Up %f %f %f\n", Up.x, Up.y, Up.z);
+            // printf("WorldUp %f %f %f\n", WorldUp.x, WorldUp.y, WorldUp.z);
+            // i = 0;
+        // }
+        // wyh 打log确认了, Up是摄像机头顶, 移动鼠标变化, WASD不变
+        // wyh WorldUp是世界上方, (0, 1, 0)一直不变
+        // wyh 但是为什么7.4给lookAt传的Up, 7.3传的WorldUp, 结果还都一样?保留伏笔!
     }
 };
 #endif
--- includes/learnopengl/mesh.h
+++ includes/learnopengl/mesh.h
@@ -14,7 +14,7 @@ using namespace std;
 
 #define MAX_BONE_INFLUENCE 4
 
-struct Vertex {
+struct Vertex { // wyh
     // position
     glm::vec3 Position;
     // normal
@@ -24,26 +24,26 @@ struct Vertex {
     // tangent
     glm::vec3 Tangent;
     // bitangent
-    glm::vec3 Bitangent;
+    glm::vec3 Bitangent; // wyh
 	//bone indexes which will influence this vertex
-	int m_BoneIDs[MAX_BONE_INFLUENCE];
+	int m_BoneIDs[MAX_BONE_INFLUENCE]; // wyh
 	//weights from each bone
-	float m_Weights[MAX_BONE_INFLUENCE];
+	float m_Weights[MAX_BONE_INFLUENCE]; // wyh
 };
 
 struct Texture {
     unsigned int id;
-    string type;
-    string path;
+    string type; // wyh texture_diffuse、texture_specular、texture_normal、texture_height
+    string path; // wyh 我们储存纹理的路径用于与其它纹理进行比较, 看是否加载过, 避免性能浪费
 };
 
-class Mesh {
+class Mesh { // wyh Mesh类
 public:
     // mesh Data
     vector<Vertex>       vertices;
     vector<unsigned int> indices;
     vector<Texture>      textures;
-    unsigned int VAO;
+    unsigned int VAO; // wyh VAO public, 2个VBO private?
 
     // constructor
     Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
@@ -57,7 +57,7 @@ public:
     }
 
     // render the mesh
-    void Draw(Shader &shader) 
+    void Draw(Shader &shader) // wyh 传入的shader有点意思, 主要也就是给uniform赋值
     {
         // bind appropriate textures
         unsigned int diffuseNr  = 1;
@@ -80,14 +80,20 @@ public:
                 number = std::to_string(heightNr++); // transfer unsigned int to string
 
             // now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i); // wyh 给这些uniform统一赋值, 这到底赋值赋的个啥?其实就是赋的sampler2D 1、2、3...
+            // uniform sampler2D texture_diffuse1; // wyh
+            // uniform sampler2D texture_diffuse2;
+            // uniform sampler2D texture_diffuse3;
+            // uniform sampler2D texture_specular1;
+            // uniform sampler2D texture_specular2;
+
             // and finally bind the texture
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
         }
         
         // draw mesh
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0); // wyh 复杂的用这个, 简单的用glDrawArrays
         glBindVertexArray(0);
 
         // always good practice to set everything back to defaults once configured.
@@ -99,12 +105,12 @@ private:
     unsigned int VBO, EBO;
 
     // initializes all the buffer objects/arrays
-    void setupMesh()
+    void setupMesh() // wyh 绑定顶点buffer, 设置顶点属性指针
     {
         // create buffers/arrays
         glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
+        glGenBuffers(1, &VBO); // wyh 注: 这个VBO里没有灯(光源)共用
+        glGenBuffers(1, &EBO); // wyh 2个VBO, 顶点数据1个, 索引1个
 
         glBindVertexArray(VAO);
         // load data into vertex buffers
@@ -112,10 +118,10 @@ private:
         // A great thing about structs is that their memory layout is sequential for all its items.
         // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
         // again translates to 3/2 floats which translates to a byte array.
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);  
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW); // wyh 传入顶点数据
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW); // wyh 传入索引数据
 
         // set the vertex attribute pointers
         // vertex Positions
@@ -123,7 +129,7 @@ private:
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
         // vertex normals
         glEnableVertexAttribArray(1);	
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal)); // wyh 使用sizeof(Vertex)、offsetof更科学
         // vertex texture coords
         glEnableVertexAttribArray(2);	
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
